625a04e13dc6f6aeb2272edcfe438343a364d42c
Afshin Safari <afshins.git@gmail.com>
2020-11-20 23:08:13 +0200

    add touch inputs to unity stardard fps controller

---
+++ Assets/Game/Player/Scripts/FPSTouchController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityStandardAssets.Characters.FirstPerson;
+using UnityEngine;
+
+public class FPSTouchController : MonoBehaviour
+{
+    public FixedJoystick FixedJoystick;
+    public FixedTouchField TouchField;
+    public FirstPersonController FPSController;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FPSController.RunAxis = FixedJoystick.Direction;
+        FPSController.m_MouseLook.LookAxis = TouchField.TouchDist;
+    }
+}
